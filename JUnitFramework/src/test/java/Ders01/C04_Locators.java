138536a995b9b3ec73ca9a5a5099e27c55144f68
Emre Tastekin <emretastekin2369@gmail.com>
2025-08-03 12:07:58 +0300

    locators öğrenildi 2

--- JUnitFramework/src/test/java/Ders01/C04_Locators.java
+++ JUnitFramework/src/test/java/Ders01/C04_Locators.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.security.Key;
 import java.time.Duration;
 import java.util.List;
 
