9f5872de5c72946a2e7d5b93a70f37cff1fe92ab
Emre Tastekin <emretastekin2369@gmail.com>
2025-08-05 17:23:32 +0300

    cssSelector öğrenildi, JUnit kısmına geçiş yapıldı.

---
+++ JUnitFramework/src/test/java/Ders01/C08_CssSelector.java
@@ -0,0 +1,63 @@
+package Ders01;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C08_CssSelector {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+
+        /*
+        cssSelector'de sadece id ve class attributeleri için locate alınabilir
+        id için basina -> # işaret konulur
+        class için başına . işareti kullanılır
+         */
+
+
+
+        //"https://the-internet.herokuapp.com/add_remove_elements/" adresine gidiniz.
+        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+
+
+        //Add element butonuna basınız
+        WebElement addElement = driver.findElement(By.xpath("//button[@onclick='addElement()']"));
+        Thread.sleep(1000);
+        addElement.click();
+
+        //Delete butonunun görünür olduğunu test ediniz
+        WebElement deleteButon = driver.findElement(By.cssSelector(".added-manually"));
+        if(deleteButon.isDisplayed()){
+            System.out.println("Test Passed");
+        }else {
+            System.out.println("Test Failed");
+        }
+
+
+        //Delete butonuna basınız
+        Thread.sleep(1000);
+        deleteButon.click();
+
+        //Add/Remove Elements yazısının görünür olduğunu kontrol ediniz
+        if(driver.findElement(By.xpath("//*[text()='Add/Remove Elements']")).isDisplayed()){
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Test Failed");
+        }
+
+        //Browser kapatıldı
+        Thread.sleep(2000);
+        driver.close();
+
+
+
+    }
+}
---
+++ JUnitFramework/src/test/java/Ders02/C02_BeforeAfter.java
@@ -0,0 +1,10 @@
+package Ders02;
+
+import org.junit.Before;
+
+public class C02_BeforeAfter {
+    @Before
+
+    public void dummy() {
+    }
+}
