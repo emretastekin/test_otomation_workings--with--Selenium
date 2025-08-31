b5b2066e7c2598662aa513746ffe5773b2d582fd
Emre Tastekin <emretastekin2369@gmail.com>
2025-08-31 13:42:30 +0300

    alerts bölümü tamamlandı

--- JUnitFramework/src/test/java/Ders03/C05_Alerts.java
+++ JUnitFramework/src/test/java/Ders03/C05_Alerts.java
@@ -2,9 +2,12 @@ package Ders03;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -26,15 +29,39 @@ public class C05_Alerts {
     }
 
     @Test
-    public void acceptAlert() {
+    public void acceptAlert() throws InterruptedException {
+        driver.findElement(By.xpath("//*[text()='Click for JS Alert']")).click();
+        Thread.sleep(1000);
+        driver.switchTo().alert().accept();
+        WebElement resultText = driver.findElement(By.xpath("//*[@id='result']"));
+        String actualText = resultText.getText();
+        String expectedText = "You successfully clicked an alert";
+        Assert.assertEquals(expectedText, actualText);
     }
 
     @Test
-    public void dismissAlert() {
+    public void dismissAlert() throws InterruptedException {
+        driver.findElement(By.xpath("//*[text()='Click for JS Confirm']")).click();
+        Thread.sleep(1000);
+        System.out.println("Alert Message : " + driver.switchTo().alert().getText());
+        driver.switchTo().alert().dismiss();
+        String actualText = driver.findElement(By.xpath("//*[text()='You clicked: Cancel']")).getText();
+        String expectedText = "successfuly";
+        Assert.assertNotEquals(expectedText, actualText);
+
+
+
     }
 
     @Test
-    public void sendKeysAlert() {
+    public void sendKeysAlert() throws InterruptedException {
+        driver.findElement(By.xpath("//*[text()='Click for JS Prompt']")).click();
+        Thread.sleep(1000);
+
+        driver.switchTo().alert().sendKeys("Emre");
+        Thread.sleep(1000);
+        driver.switchTo().alert().accept();
+
     }
 
 }
